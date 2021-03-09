a6de59998b60b3ddc08291e068a54d59de1c6d5b
adityakrmaurya <adityakrmaurya03@gmail.com>
2021-03-09 20:16:52 +0530

    feat: Write a program to get all pid

--- main.cc
+++ main.cc
@@ -1,13 +1,14 @@
-#include <dirent.h>
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
 #include <algorithm>
+#include <cctype>
 #include <cerrno>
 #include <chrono>
 #include <cstring>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <iterator>
@@ -23,13 +24,15 @@ class ProcessParser {
  public:
   // Return the cmdline of the given `pid`
   // cmdline - This is read-only file holds complete command line for process,
-  // unless the process is a zombie.  In the latter case, there is nothing in
-  // this file: that is, a read on this file  will return 0 characters. The
+  // unless the process is a zombie. In the latter case, there is nothing in
+  // this file: that is, a read on this file will return 0 characters. The
   // commandline arguments appear in this file as a set of strings separated by
   // null bytes('\0'), with a further null byte after the last string.
   static std::string GetCmd(std::string pid);
-
+  // Return all the pid in a vector. In proc folder directory name(integral) is
+  // the pid. The program grasp those directory name.
   static std::vector<std::string> GetPidList();
+  //
   static std::string GetVmSize(std::string pid);
   static std::string GetCpuPercent(std::string pid);
   static long int GetSysUpTime();
@@ -52,4 +55,23 @@ std::string ProcessParser::GetCmd(std::string pid) {
   std::getline(stream, command);
   return command;
 }
-int main(void) { std::cout << ProcessParser::GetCmd("1951"); }
+std::vector<std::string> ProcessParser::GetPidList() {
+  // stores the pid
+  std::vector<std::string> vs;
+  // iterate
+  
+  for (auto& p : std::filesystem::directory_iterator(Path::BasePath())) {
+    if (p.is_directory() && std::isdigit(p.path().filename().string()[0])) {
+      vs.push_back(p.path().filename().string());
+    }
+  }
+  return vs;
+}
+int main(void) {
+  // simple tests
+  std::cout << ProcessParser::GetCmd("1") << std::endl;
+  std::vector<std::string> vs = ProcessParser::GetPidList();
+  for (auto& a : vs) {
+    std::cout << a << std::endl;
+  }
+}
